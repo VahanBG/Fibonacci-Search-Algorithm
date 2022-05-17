b9612fbf9dbe45f9c597e071505adb910264b960
VahanBG <vahan365105@gmail.com>
2022-05-17 19:26:54 +0400

    next commit

--- binar_search.cpp
+++ binar_search.cpp
@@ -1,8 +1,9 @@
 // Փնտրում ենք մեր ուզած էլեմնտը,
 //եթե առկա է զանգվածում ՝ վերադարցնում ենք ինդեքսը, եթե ոճ ՝ -1 ։
 #include<iostream>
+#include<vector>
 #include <algorithm>
-void sort ( int* arr , int size_arr )
+void sort ( std::vector<int>::iterator arr , int size_arr )
 {
     for(int i = 0 ; i < size_arr - 1 ; i++){
         for( int j = i + 1 ; j < size_arr ; j++){
@@ -13,7 +14,7 @@ void sort ( int* arr , int size_arr )
     }
 }
 
-int search_binar(int arr[],int size,int number){
+int search_binar(std::vector<int>::iterator arr,int size,int number){
     int first_index = 0 ; //pntrvox arachin element
     int last_index = size - 1 ;//pntrvox verchin element
     while( first_index <= last_index ){
@@ -31,7 +32,7 @@ int search_binar(int arr[],int size,int number){
           return -1 ;
 }
 
-void print_arr ( int *arr , int size_arr){
+void print_arr ( std::vector<int>::iterator arr , int size_arr){
   for( int i = 0 ; i < size_arr; i++ ){
         std::cout << arr[i] << " " ;
     }
@@ -41,8 +42,19 @@ void print_arr ( int *arr , int size_arr){
 
 int main()
 {
-     int arr[]= {2,5,7,45,18,13,78,65,12,44};
-     int size = sizeof(arr) / sizeof(arr[0]);
+     //int arr[]= {2,5,7,45,18,13,78,65,12,44};
+     int size;// = sizeof(arr) / sizeof(arr[0]);
+     std::cout<<"please enter size of numbers in your array"<<std::endl;
+     std::cin >> size ;
+     std::cout<<"please enter numbers of your array"<<std::endl;
+     std::vector <int> myVector;
+     int numbers ;
+     for(int i = 0 ; i < size ; i++){
+         std::cout<<"enter value of "<< i << " index " <<std::endl ;
+         std::cin>> numbers;
+         myVector.push_back(numbers);
+         numbers = 0 ;
+     }
      int number;
      std::cout<<"please enter the number which are you search"<<std::endl;
      std::cin>>number;
@@ -50,8 +62,11 @@ int main()
      //std::cout<<"the number index in your arr is : "<<search(arr,size,number); 
      //
      //else{std::cout<<"the number which you are serarching not in your arr";}
-     sort(arr,size);
-     //print_arr(arr,size);//tpum e zangvac@
-     std::cout<<search_binar(arr,size,number);
+     std::vector<int>::iterator it;
+     it = myVector.begin();
+     sort(it,size);
+     print_arr(it,size);//tpum e zangvac@
+     //std::cout<<search_binar(myVector,size,number);
+     
      
 }
